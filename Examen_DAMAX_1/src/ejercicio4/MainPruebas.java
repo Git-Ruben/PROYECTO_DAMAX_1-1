6ea31bf6bf388101a5c342fc12395123fbd4a745
KimboLeon <flkratos22@gmail.com>
2021-03-23 12:55:02 +0100

    Se ha añadido una clase llamada MainPruebas que contendra el metodo main y se ha añadido un comentario que explica las pruebas a realizar en dicha clase.

---
+++ Examen_DAMAX_1/src/ejercicio4/MainPruebas.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio4;
+
+/**
+ * Clase MainPruebas
+ *
+ * Esta clase contendra el metodo main necesario para ejecutar el programa,
+ * en este haremos las siguientes pruebas:
+ *
+ * - Crear un paciente de cada tiempo.
+ * 
+ * - Insertarlos en la colección (el ultimo de ello que no lo inserte porque
+ *   haya otro de menor prioridad ya dentro de la colección).
+ * 
+ * - Llama ál metodo que muestre la informacion de la lista.
+ * 
+ * - Vacuna a un paciente de manera exitosa.
+ * 
+ * - Vacuna a un paciente provocando una excepción.
+ *
+ * @author AnthonyLeonLucero
+ */
+public class MainPruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
