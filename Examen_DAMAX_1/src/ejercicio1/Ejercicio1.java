ce4f54d3ae14e6f89d5f92295397fed21957fafd
DAM1 <DAM1@DAMPC15>
2021-03-23 13:19:29 +0100

    Commit_1 Diego

---
+++ Examen_DAMAX_1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+MI ANÁLISIS DE LA PREGUNTA 1.
+Pregunta 1: es un ejercicio de ordenación y búsqueda, que necesita la parte de la ordenación para 
+empezar con el apartado de la búsqueda. El enunciado me pide ordenar cadenas de caracteres por
+orden alfabético, utilizarlo y realizar una búsqueda; en mi caso lo resolveré usando el método de
+la burbuja.
+
+ */
+public class Ejercicio1 {
+    
+    
+    public void ordenacionDiego(){
+        //método burbuja
+        
+        
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        //pruebas con los metodos
+        
+        
+    }
+    
+}
